d18218eee99a7be68740a1a4264873aea6e784ef
Mayson Said <116666638+maysson19@users.noreply.github.com>
2024-09-04 13:49:30 +0300

    Add files via upload

---
+++ Collections java trainee/collec/src/Main.java
@@ -0,0 +1,116 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+//        Map map=new HashMap();
+//        map.put(1,"Mayson");
+//        map.put(5,"Raghd");
+//        map.put(2,"Zeianb");
+//        map.put(6,"Ahmed");
+//        Set set=map.entrySet();
+//        Iterator itr=set.iterator();
+//        while(itr.hasNext()){
+//            Map.Entry entry=(Map.Entry)itr.next();
+//            System.out.println(entry.getKey()+" "+entry.getValue());
+//        }
+//////////////////////////////////////////
+
+//        HashSet<String> set=new HashSet<String>();
+//        set.add("Raghd");
+//        set.add("Mohamed");
+//        set.add("Mayson");
+//        set.add("Ahmed");
+//        System.out.println("An initial list of elements: "+set);
+//
+//        set.remove("Ravi");
+//        System.out.println("After invoking remove(object) method: "+set);
+//        HashSet<String> set1=new HashSet<String>();
+//        set1.add("Ahmed");
+//        set1.add("Mayson");
+//        set.addAll(set1);
+//        System.out.println("Updated List: "+set);
+//
+//        set.removeAll(set1);
+//        System.out.println("After invoking removeAll() method: "+set);
+//
+//        set.removeIf(str->str.contains("Vijay"));
+//        System.out.println("After invoking removeIf() method: "+set);
+//
+//        set.clear();
+//        System.out.println("After invoking clear() method: "+set);
+
+
+/////////////////////////////////////////
+
+//        TreeSet<String> set=new TreeSet<String>();
+//        set.add("Mayson");
+//        set.add("Raghd");
+//        set.add("Zaienb");
+//        Iterator<String> tes= set.iterator();
+//        while (tes.hasNext()){
+//            System.out.println(tes.next());
+//        }
+
+
+
+////////////////////////////////////////////
+//        LinkedHashSet<String> set=new LinkedHashSet<String>();
+//        set.add("Mayson");
+//        set.add("Raghd");
+//        set.add("Zaienb");
+//        Iterator<String> tes= set.iterator();
+//        while (tes.hasNext()){
+//            System.out.println(tes.next());
+//        }
+
+
+
+///////////////////////////////////////////////
+//        HashSet<String> set=new HashSet<String>();
+//        set.add("Mayson");
+//        set.add("Raghd");
+//        set.add("Zaienb");
+//        Iterator<String> tes= set.iterator();
+//        while (tes.hasNext()){
+//            System.out.println(tes.next());
+//        }
+
+
+/////////////////////////////////
+//        LinkedList<String> list=new LinkedList<String>();
+//        list.add("Mayson");
+//        list.add("Raghd");
+//        list.add("Zaienb");
+//        Iterator<String> tes= list.iterator();
+//        while (tes.hasNext()){
+//            System.out.println(tes.next());
+//        }
+
+
+
+        ////////////////////////////////////////////////////////
+//        Vector<String> list=new Vector<String>();
+//        list.add("Mayson");
+//        list.add("Raghd");
+//        list.add("Zaienb");
+//        Iterator<String> tes= list.iterator();
+//        while (tes.hasNext()){
+//            System.out.println(tes.next());
+//        }
+
+///////////////////////////////////////////////////////////////
+
+//        ArrayList<String> list=new ArrayList<String>();
+//        list.add("Mayson");
+//        list.add("Raghd");
+//        list.add("Zaienb");
+//        Iterator tes= list.iterator();
+//        while (tes.hasNext()){
+//            System.out.println(tes.next());
+//        }
+    }
+}
+//////////////////////////////////////////
